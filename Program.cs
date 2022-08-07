cf41ee821a370d585a894434f28418734374d998
mani1 <mani1@NANI143>
2022-08-07 20:02:40 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AddressBookProblemUC3
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Contact person = new Contact();
+
+            PersonAddress GetPerson = new PersonAddress();
+            GetPerson.Book();
+            GetPerson.Show();
+            GetPerson.EditContact();
+            GetPerson.RemoveContact();
+        }
+    }
+}
