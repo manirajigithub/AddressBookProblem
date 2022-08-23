df0841a8e5af1f17bb307dcb0900643ec95693a7
mani1 <mani1@NANI143>
2022-08-23 21:31:13 +0530

    [Rajeswari]Added New Contacts

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
-namespace AddressBookProblemUC5
+namespace AddressBookProblemUC8
 {
     class program
     {
+        private static object addressBook;
+
         public static void Main(string[] args)
         {
             Contact person = new Contact();
             Console.WriteLine("Enter the City Name for Searchin Person City in Dictionary ");
             string cityName = Console.ReadLine();
-            addressBook.SearchingPersonInCity(cityName);
+            object value = addressBook.SearchingPersonInCity(cityName);
         }
     }
 }
