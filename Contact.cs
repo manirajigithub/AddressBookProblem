f4f181a21e123a615281fa35bb9683cd0e7b0195
mani1 <mani1@NANI143>
2022-08-26 21:19:16 +0530

    [Rajeswari]Added Sort City State in Contact

---
+++ Contact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblemUC12
+{
+    internal class Contact
+    {
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public int zipcode;
+        public long phoneNumber;
+        public string email;
+
+        public Contact(string firstName, string lastName, string address, string city, string state, int zipcode, long phoneNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipcode = zipcode;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+        public override string ToString()
+        {
+            return $"{firstName} {lastName} {address} {city} {state} {zipcode} {phoneNumber} {email}";
+        }
+    }
+}
