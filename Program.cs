bb7e48b81ee14b9d8c42998d0a4b4c16503eaa5c
manirajigithub <109149403+manirajigithub@users.noreply.github.com>
2022-08-26 21:55:24 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AddressBookProblemUc11;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,13 +15,12 @@ namespace AddressBookProblemUC11
         {
             Console.WriteLine("Welcome to Address Book Program\n");
 
-            PersonAddress personAddress = new PersonAddress();
-            personAddress.AddPersonAddress("Default");
-            personAddress.AddContactDetails("Raji", "Balusupati", "Kothali", "guntur", "Andhra", 522003, 8790653371, "raji123.dev@gmail.com", "Default");
-            personAddress.AddContactDetails("Manasa", "Balu", "koti", "HyderabaD", "telegnana", 570037, 9543651869, "Rajeswari@gmail.com", "Default");
-            personAddress.AddContactDetails("ganga", "Naru", "gundur", "Banglore", "Maharastra", 5220037, 912165789, "Rahu@gmail.com", "Default");
 
-            while (true)
+            AddressBookSystem addressBook = new AddressBookSystem();
+            addressBook.AddAddressBook("Default");
+            addressBook.AddContactDetails("Raji", "Balusupati", "Kothali", "guntur", "Andhra", 522003, 8790653371, "raji123.dev@gmail.com", "Default");
+            addressBook.AddContactDetails("Manasa", "Balu", "koti", "HyderabaD", "telegnana", 570037, 9543651869, "Rajeswari@gmail.com", "Default");
+            addressBook.AddContactDetails("ganga", "Naru", "gundur", "Banglore", "Maharastra", 5220037, 912165789, "Rahu@gmail.com", "default");
             {
                 try
                 {
@@ -33,44 +33,44 @@ namespace AddressBookProblemUC11
                         case 1:
                             Console.WriteLine("Enter Unique Address Book Name to create: ");
                             string newBookName = Console.ReadLine();
-                            personAddress.AddPersonAddress(newBookName);
-                            if (personAddress.CheckPersonAddress(newBookName) == newBookName)
+                            addressBook.AddAddressBook(newBookName);
+                            if (addressBook.CheckAddressBook(newBookName) == newBookName)
                             {
                                 bookName = newBookName;
                                 Console.WriteLine($"Switched to: {bookName}");
                             }
                             break;
                         case 2:
-                            personAddress.AddNewContact(bookName);
+                            addressBook.AddNewContact(bookName);
                             break;
                         case 3:
-                            personAddress.ViewContacts(bookName);
+                            addressBook.ViewContacts(bookName);
                             break;
                         case 4:
                             Console.WriteLine("Enter the First Name to View Contact Details: ");
                             string f_Name = Console.ReadLine();
-                            personAddress.ViewContact(f_Name, bookName);
+                            addressBook.ViewContact(f_Name, bookName);
                             break;
                         case 5:
                             Console.WriteLine("Enter the First Name to Edit Contact Details: ");
                             string input = Console.ReadLine();
-                            personAddress.EditContact(input, bookName);
-                            personAddress.ViewContact(input, bookName);
+                            addressBook.EditContact(input, bookName);
+                            addressBook.ViewContact(input, bookName);
                             break;
                         case 6:
                             Console.WriteLine("Enter the First Name of Contact: ");
                             string fName = Console.ReadLine();
                             Console.WriteLine("Enter the Last Name to Delete Contact: ");
                             string lName = Console.ReadLine();
-                            personAddress.DeleteContact(fName, lName, bookName);
+                            addressBook.DeleteContact(fName, lName, bookName);
                             break;
                         case 7:
-                            personAddress.ViewAddressBooks();
+                            addressBook.ViewAddressBooks();
                             break;
                         case 8:
                             Console.WriteLine("Enter the AddressBook Name to Sitch into: ");
                             string adBookName = Console.ReadLine();
-                            if (personAddress.CheckAddressBook(adBookName) == adBookName)
+                            if (addressBook.CheckAddressBook(adBookName) == adBookName)
                             {
                                 bookName = adBookName;
                                 Console.WriteLine($"Switched to: {bookName}");
@@ -81,19 +81,19 @@ namespace AddressBookProblemUC11
                         case 9:
                             Console.WriteLine("Enter the city or state to Search person by city or state across addressbook: ");
                             string userData = Console.ReadLine();
-                            personAddress.SearchPersonByCityOrState(userData);
+                            addressBook.SearchPersonByCityOrState(userData);
                             break;
                         case 10:
                             Console.WriteLine("View person by city or state across addressbook: ");
-                            personAddress.ViewPersonByCityOrState();
+                            addressBook.ViewPersonByCityOrState();
                             break;
                         case 11:
                             Console.WriteLine("person count by city or state: ");
-                            personAddress.CountPersonByCityOrState();
+                            addressBook.CountPersonByCityOrState();
                             break;
                         case 12:
                             Console.WriteLine("\nSort by person Name:");
-                            personAddress.SortByName(bookName);
+                            addressBook.SortByName(bookName);
                             break;
                         case 13:
                             Environment.Exit(0);
